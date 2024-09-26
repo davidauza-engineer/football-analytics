56038b6931b5289a6036ef772426a0c775cf72f1
David Auza <davidauza01@gmail.com>
2024-09-26 02:59:56 -0500

    feat(instances): complete topic

---
+++ Topics/Class instances/Pumpkin/main.py
@@ -0,0 +1,5 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+pumpkin_dog = Dog(name="Pumpkin")
---
+++ Topics/Class instances/Shopping/main.py
@@ -0,0 +1,7 @@
+class Store:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+shop = Store("GAP", "clothes")
+print(shop.name, shop.category)
