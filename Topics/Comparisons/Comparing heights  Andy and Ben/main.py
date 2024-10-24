c9e5ad96ccafbb79351a7cc3651346096564d810
David Auza <davidauza01@gmail.com>
2024-10-24 05:22:00 -0500

    feat(comparisons): complete topic

---
+++ Topics/Comparisons/Comparing heights  Andy and Ben/main.py
@@ -0,0 +1,11 @@
+# To get input from the user, use the input function, which returns the entered value as a string.
+# To convert this string to an integer, use the built-in int function.
+
+andy_height = int(input())
+ben_height = int(input())
+
+# Now, write a comparison operation to check if Andy's height is greater than Ben's. 
+# Remember, the comparison operator should return a boolean value: True or False.
+
+# Finally, print the result of the comparison operation.
+print(andy_height > ben_height)
---
+++ Topics/Comparisons/Guessing game/main.py
@@ -0,0 +1,6 @@
+set_number = 6557
+
+number_one = int(input())
+number_two = int(input())
+
+print((number_one * number_two) == set_number)
